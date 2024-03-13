6cedf6228fcdd2068784d67406b687a8d6c237ac
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2024-03-13 05:54:22 +0100

    A simple program that converts temperatures from Fahrenheit to Celcius and vice versa

---
+++ temperature converter/script.js
@@ -0,0 +1,19 @@
+const textBox = document.getElementById("textBox")
+const toFah = document.getElementById("toFahrenheit")
+const toCel = document.getElementById("toCelcius")
+const result = document.getElementById("result")
+let temp
+
+function convert() {
+    if(toFah.checked) {
+        temp = Number(textBox.value)
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
+    } else if(toCel.checked) {
+        temp = Number(textBox.value)
+        temp = (temp - 32) * (5/9)
+        result.textContent = temp.toFixed(1) + "°C"
+    } else {
+        result.textContent = "Select a unit"
+    }
+}
